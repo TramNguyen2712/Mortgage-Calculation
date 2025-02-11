838f4c86f6b69e4972ee1cbad300f2e2f1044b0d
TramNguyen2712 <tramnguyen27121996@gmail.com>
2025-02-11 10:14:39 -0600

    Final version

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 import io
 import base64
 
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -41,11 +42,9 @@ def amortization(remain_balance, extra, interest_rate, years, start_year,start_m
 
     total_principal = 0 
 
-    #Calculate save amount if user inputs extra money. 
+    #Calculate save months if user inputs extra money. 
 
-    total_interest_saved = 0
-
-    months_saved = 0
+    months_paid = 0
     
     i = (interest_rate/100)/12
     
@@ -71,15 +70,14 @@ def amortization(remain_balance, extra, interest_rate, years, start_year,start_m
 
         total_interest += interest
         total_principal += principal
-        total_interest_saved += interest
 
         monthly_schedule.append({
             "Month": month,
-            "Payment": round(monthly_payment,2),
+            "Payment": round(monthly_payment,0),
             "Date": current_date.strftime("%Y-%m"),
-            "Interest": round(interest,2),
-            "Principal": round(principal,2),
-            "Remaining Balance": round(remain_balance,2)
+            "Interest": round(interest,0),
+            "Principal": round(principal,0),
+            "Remaining Balance": round(remain_balance,0)
         })
         
         year_summary["Total Payment"] += total_payment
@@ -93,11 +91,18 @@ def amortization(remain_balance, extra, interest_rate, years, start_year,start_m
 
         if next_month == 1:  # At the start of a new year, store the previous year's summary
             year_summary["Remaining Balance"] = remain_balance
-            yearly_schedule.append(year_summary)
+            yearly_schedule.append({
+                "Year": year_summary["Year"],
+                "Total Payment": round(year_summary["Total Payment"]),
+                "Total Interest": round(year_summary["Total Interest"],0),
+                "Total Principal": round(year_summary["Total Principal"],0),
+                "Remaining Balance": round(year_summary["Remaining Balance"],0)
+            })
+            
             year_summary = {"Year": next_year, "Total Payment": 0, "Total Interest": 0, "Total Principal": 0, "Remaining Balance": remain_balance}
 
 
-        months_saved += 1
+        months_paid += 1
 
         #Stop early when remain balance is paid off
         if remain_balance <= 0:
@@ -106,20 +111,27 @@ def amortization(remain_balance, extra, interest_rate, years, start_year,start_m
     final_month = covert_number_to_month(current_date.month)
     final_year = current_date.year
 
-    return monthly_schedule,yearly_schedule,months_saved,total_interest_saved,total_interest,total_principal,final_month,final_year
+    months_saved = n - months_paid
+
+    return monthly_schedule,yearly_schedule,months_saved,total_interest,total_principal,final_month,final_year
 
 def generate_pie_chart(total_principal,total_interest):
     if total_interest <=0 or total_principal <=0:
         return "Error: Total interest or principal is zero"
     
     fig = Figure()
+    fig.set_facecolor("#f4f1ec")
     ax = fig.add_subplot(111)
     ax.pie(
         [total_principal, total_interest],
         labels=['Principal', 'Interest'],
-        autopct='%1.1f%%',
+        autopct='%1.0f%%',
         startangle=90,
-        colors=['#66b3ff', '#ff9999']
+        colors=['#83b8a1','#ffbd59'],
+        textprops={'fontsize': 16, 'fontweight': 'bold'},
+        wedgeprops= {"edgecolor":"#f4f1ec", 
+                     'linewidth': 4, 
+                     'antialiased': True}
     )
     ax.axis('equal')
 
@@ -131,19 +143,20 @@ def generate_pie_chart(total_principal,total_interest):
 def generate_line_chart(schedule):
     df = pd.DataFrame(schedule)
     fig = Figure()
+    fig.set_facecolor("#f4f1ec")
     ax = fig.add_subplot(1, 1, 1)
+    ax.set_facecolor("#f4f1ec")
 
     df['Cumulative Interest'] = df['Total Interest'].cumsum()
     df['Cumulative Payment'] = df['Total Payment'].cumsum() + df['Cumulative Interest']
 
-    ax.plot(df['Year'], df['Cumulative Payment'], label="Payment Paid", color="blue")
-    ax.plot(df['Year'], df['Cumulative Interest'], label="Interest Paid", color="red")
-    ax.plot(df['Year'], df['Remaining Balance'], label="Remaining Balance", color="green")
+    ax.plot(df['Year'], df['Cumulative Payment'], label="Payment Paid", color="#83b8a1",linewidth=3)
+    ax.plot(df['Year'], df['Cumulative Interest'], label="Interest Paid", color="#ffbd59",linewidth=3)
+    ax.plot(df['Year'], df['Remaining Balance'], label="Remaining Balance", color="#000000",linewidth=2)
 
-    ax.set_xlabel("Years")
-    ax.set_ylabel("Amount ($)")
-    ax.set_title("Loan Breakdown Over Time")
-    ax.legend()
+    ax.set_xlabel("Years", fontsize=12)
+    ax.set_ylabel("Amount ($)", fontsize=12)
+    ax.legend(fontsize=12,facecolor="#f4f1ec")
 
     img = io.BytesIO()
     FigureCanvas(fig).print_png(img)  # Save the figure in memory
@@ -162,6 +175,7 @@ def handle_action():
         return "Invalid action."
 
 def calculate(request):
+    
     try:
         # Get the values from the form
         amount1 = float(request.form['amount1'])
@@ -169,7 +183,7 @@ def calculate(request):
         years1 = int(request.form['years1'])
         down1 = float(request.form.get('down1', 0))
         extra1 = float(request.form.get('extra1', 0))
-        start_month1 = request.form['start_month1']
+        start_month1 = int(request.form['start_month1'])
         start_year1 = request.form['start_year1']
 
         # Calculate the interest
@@ -177,7 +191,7 @@ def calculate(request):
         
         monthly_payment = calculate_monthly_payment(loan_amount,rate1,years1)
 
-        monthly_schedule,yearly_schedule,month_saved,total_interest_saved,total_interest,total_principal,final_month,final_year = amortization(loan_amount,extra1,rate1,years1,start_year1,start_month1)
+        monthly_schedule,yearly_schedule,month_saved,total_interest,total_principal,final_month,final_year = amortization(loan_amount,extra1,rate1,years1,start_year1,start_month1)
 
         pie_chart = generate_pie_chart(round(total_principal,2),round(total_interest,2))
 
@@ -185,14 +199,15 @@ def calculate(request):
 
         total_mortgage_payments = total_interest + loan_amount
         
-        return render_template('result.html', monthly_payment=round(monthly_payment,2), loan_amount = loan_amount, total_interest = round(total_interest,2), down = down1, 
-                               total_mortgage_payments = round(total_mortgage_payments,2), total_interest_saved=round(total_interest_saved, 2),month_saved = month_saved, monthly_schedule = monthly_schedule, 
+        return render_template('result.html', monthly_payment=f"{monthly_payment:,.0f}", loan_amount = f"{loan_amount:,.0f}", total_interest = f"{total_interest:,.0f}", down = f"{down1:,.0f}", 
+                               total_mortgage_payments = f"{total_mortgage_payments:,.0f}",month_saved = month_saved, monthly_schedule = monthly_schedule, 
                                yearly_schedule = yearly_schedule, final_month=final_month,final_year=final_year,pie_chart=pie_chart,line_chart=line_chart)
 
     except Exception as e:
         return f"Error: {e}"
     
 def compare(request):
+    
     try:
         # Scenario 1 Inputs
         amount1 = float(request.form['amount1'])
@@ -215,13 +230,13 @@ def compare(request):
         # Calculate for Scenario 1
         loan_amount1 = amount1 - down1
         monthly_payment1 = calculate_monthly_payment(loan_amount1, rate1, years1)
-        monthly_schedule1,yearly_schedule1,month_saved1,total_interest_saved1,total_interest1,total_principal1,final_month1,final_year1 = amortization(loan_amount1,extra1,rate1,years1,start_year1,start_month1)
+        monthly_schedule1,yearly_schedule1,month_saved1,total_interest1,total_principal1,final_month1,final_year1 = amortization(loan_amount1,extra1,rate1,years1,start_year1,start_month1)
 
 
         # Calculate for Scenario 2
         loan_amount2 = amount2 - down2
         monthly_payment2 = calculate_monthly_payment(loan_amount2, rate2, years2)
-        monthly_schedule2,yearly_schedule2,month_saved2,total_interest_saved2,total_interest2,total_principal2,final_month2,final_year2 = amortization(loan_amount2,extra2,rate2,years2,start_year2,start_month2)
+        monthly_schedule2,yearly_schedule2,month_saved2,total_interest2,total_principal2,final_month2,final_year2 = amortization(loan_amount2,extra2,rate2,years2,start_year2,start_month2)
 
 
         # Create charts for both scenarios
@@ -231,15 +246,21 @@ def compare(request):
         # Render the comparison results
         return render_template(
             'compare.html',
-            monthly_payment1=round(monthly_payment1, 2),
-            total_interest1=round(total_interest1, 2),
+            monthly_payment1=f"{monthly_payment1:,.0f}",
+            total_interest1=f"{total_interest1:,.0f}",
             months_saved1=month_saved1,
             pie_chart1=pie_chart1,
+            yearly_schedule1 = yearly_schedule1,
+            final_month1=final_month1,
+            final_year1=final_year1,
 
-            monthly_payment2=round(monthly_payment2, 2),
-            total_interest2=round(total_interest2, 2),
+            monthly_payment2=f"{monthly_payment2:,.0f}",
+            total_interest2=f"{total_interest2:,.0f}",
             months_saved2=month_saved2,
             pie_chart2=pie_chart2,
+            yearly_schedule2 = yearly_schedule2,
+            final_month2=final_month2,
+            final_year2=final_year2,
         )
     except Exception as e:
         return f"Error: {e}"
